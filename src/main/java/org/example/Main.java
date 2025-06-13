f3d8bacec0c5d64f14044be7c5b77dfb203259e8
Lev <grablin-lev@yandex.ru>
2025-06-13 09:02:52 +0300

    Первое практическое задание(четные и нечетные числа)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,14 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+// Метод позволяющий определить, четное ли число поступило на входе
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        checkInt();
+    }
+    public static void checkInt(){
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        String info = (a % 2 == 0) ? "число четное" : "число не четное";
+        System.out.println(info);
     }
 }
